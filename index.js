120b2cd940e44cb78140bd5782e1920c9690436e
sanchezguidorl <sanchezguidorl@gmail.com>
2023-10-18 08:26:14 -0300

    Se realiza modificación sobre el orden de métodos en el archivo index

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const app=require("./app/app.js");
 const connection= require("./app/db/db.js");
 
 //Método inicializador de la app
-const main= ()=>{
-app.listen(5000, ()=>{
-console.log("Servidor iniciado en el puerto 5000")
-connection();
-});
+const main=async()=>{
+  await connection();
 }
 
-
-
 main();
+
+app.listen(5000, ()=>{
+console.log("Servidor iniciado en el puerto 5000")
+});
