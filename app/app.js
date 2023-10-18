ea38411316bab895fa625e4477ed0658f2545889
sanchezguidorl <sanchezguidorl@gmail.com>
2023-10-18 07:34:38 -0300

    Se agrega archivo controlador de acciones para las rutas asignadas en el archivo de rutas

--- app/app.js
+++ app/app.js
@@ -1,7 +1,11 @@
-const express= require("express");
-const app= express();
-const personsRoutes= require("./routes/persons.routes.js");
+const express = require("express");
+const app = express();
+const personsRoutes = require("./routes/persons.routes.js");
+const cors = require('cors');
 
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({extended: true}));
 app.use(personsRoutes);
-module.exports= app;
+
+module.exports = app;
